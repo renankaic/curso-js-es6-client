554727178e5276910fb2f14674b73097633524e4
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-10-29 10:56:41 -0300

    Criando classe Negociação

---
+++ js/app/models/Negociacao.js
@@ -0,0 +1,16 @@
+class Negociacao {
+
+    constructor() {
+
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0;
+
+    }
+
+    obtemVolume() {
+
+        return this.quantidade * this.valor;
+
+    }
+}
