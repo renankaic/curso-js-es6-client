16e395328403badb3dd1f09a9b9e7bde52d53609
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-10-30 16:59:24 -0300

    Criando objeto data. Usando Spread e arrow function

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -3,15 +3,27 @@ class NegociacaoController {
     constructor() {
 
         let $ = document.querySelector.bind(document);
-        this.inputData = $("#data");
-        this.inputQuantidade = $("#quantidade");
-        this.inputValor = $("#valor");
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
 
     }
 
     adiciona(event){
 
         event.preventDefault();
+
+        let data = new Date( 
+            ...this._inputData.value
+                   .split('-')
+                   .map((item, indice) => item - indice % 2) 
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
 
     }
 
