35b1eac73e69ae0453348127cdbb2bfdc9bed558
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-10-30 15:07:43 -0300

    Evitando de percorrer o DOM usando o constructor na classe NegociacaoController

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,14 +1,17 @@
 class NegociacaoController {
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        this.inputData = $("#data");
+        this.inputQuantidade = $("#quantidade");
+        this.inputValor = $("#valor");
+
+    }
+
     adiciona(event){
 
         event.preventDefault();
-
-        let $ = document.querySelector.bind(document);
-        let inputData = $("#data");
-        let inputQuantidade = $("#quantidade");
-        let inputValor = $("#valor");
-
 
     }
 
