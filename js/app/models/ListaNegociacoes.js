3754702ecd4c530ca23a3cff9ed001af9dd23879
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-10-31 13:48:16 -0300

    Usando programação defensiva para impedir alterações na ModelList

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -8,6 +8,6 @@ class ListaNegociacoes {
 
     adiciona(negociacao) { this._negociacoes.push(negociacao); }
 
-    get negociacoes() { return this._negociacoes; }
+    get negociacoes() { return [].concat(this._negociacoes); }
 
 }
