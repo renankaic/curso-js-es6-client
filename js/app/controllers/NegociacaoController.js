260dad2fc08f35b7be0ad2f5bad5e8804ffe39d3
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-10-31 10:05:32 -0300

    Criando um ListModel para listar as negociações. Refatoração de código.

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -6,6 +6,7 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
@@ -13,14 +14,30 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-        )
+        );
 
-        let dataMesAno = DateHelper.dataParaTexto(negociacao.data);
-        console.log(dataMesAno);
+    }
+
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
 
     }
 
