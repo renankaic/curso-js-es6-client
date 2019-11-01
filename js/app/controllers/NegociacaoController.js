962bcbdf21bd18fe52ec3ea5495f7dbd32ae0e52
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-11-01 08:51:05 -0300

    Retornando uma tabela com os reegistros para a view

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -9,8 +9,8 @@ class NegociacaoController {
         this._listaNegociacoes = new ListaNegociacoes();
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update();
-        
+        this._negociacoesView.update(this._listaNegociacoes);
+
     }
 
     adiciona(event){
@@ -18,9 +18,8 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes);
 
     }
 
--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -6,7 +6,7 @@ class NegociacoesView {
 
     }
 
-    _template() {
+    _template(model) {
 
         return `
         <table class="table table-hover table-bordered">
@@ -20,6 +20,16 @@ class NegociacoesView {
             </thead>
             
             <tbody>
+                ${model.negociacoes.map(n => 
+                    `
+                        <tr>
+                            <td>${DateHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                        </tr>
+                    `    
+                ).join('')}
             </tbody>
             
             <tfoot>
@@ -29,9 +39,9 @@ class NegociacoesView {
 
     }
 
-    update() {
+    update(model) {
 
-        this._elemento.innerHTML = this._template();
+        this._elemento.innerHTML = this._template(model);
 
     }
 
