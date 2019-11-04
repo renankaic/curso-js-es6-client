5f7b654103813cf4ff9d4d3387ed49c0fc0ae774
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-11-04 11:47:15 -0300

    Adicionando MensagemView para mostrar uma mensagem apos adicionar uma negociacao

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,6 +11,9 @@ class NegociacaoController {
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
 
+        this._mensagemView = new MensagemView($("#mensagemView"));
+        this._mensagem = new Mensagem();
+
     }
 
     adiciona(event){
@@ -19,6 +22,10 @@ class NegociacaoController {
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = "Negociação adicionada com sucesso!";
+        this._mensagemView.update( this._mensagem );
+
         this._limpaFormulario();
 
     }
