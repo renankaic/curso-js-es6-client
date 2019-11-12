4da2e924acefeb17cc278bb22039b78a9b36e486
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-11-12 14:51:34 -0300

    Usando o Proxy para interceptar métodos no model

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -3,12 +3,29 @@ class NegociacaoController {
     constructor() {
 
         let $ = document.querySelector.bind(document);
+        let self = this;
+
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
 
-        this._listaNegociacoes = new ListaNegociacoes(model =>  {
-            this._negociacoesView.update(model);
+            get(target, prop, receiver) {
+
+                if(["adiciona","esvazia"].includes(prop) && typeof(target[prop]) == typeof(Function)){
+
+                    return function() {
+                        console.log(`Interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+
+                }
+
+                return Reflect.get(target, prop, receiver);
+
+            }
+
         });
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
