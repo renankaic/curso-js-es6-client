011754dc8c456366734eda16e0a19af08ae97a93
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-11-14 16:41:13 -0300

    Adicionando suporte à importação de negociações da semana

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -33,6 +33,47 @@ class NegociacaoController {
 
     }
 
+    importaNegociacoes() {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("GET","negociacoes/semana");
+
+        xhr.onreadystatechange = () => {
+
+            /*
+                0: requisição ainda não iniciada
+                1: conexão com o servidor estabelecida
+                2: requisição recebida
+                3: processando requisição
+                4: requisição concluída e a resposta está pronta 
+            */
+
+            if (xhr.readyState === 4 ) {
+
+                if (xhr.status === 200 ) {
+
+                    JSON.parse(xhr.responseText)
+                        .map( objeto =>  new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor) )
+                        .forEach( negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    
+                    this._mensagem.texto = "Negociações importadas com sucesso!";
+
+                } else {
+
+                    console.error(xhr.responseText);
+                    this._mensagem.texto = "Não foi possível obter as negociações da semana";
+
+                }
+
+            }
+
+        };
+
+        xhr.send();
+
+    }
+
     _criaNegociacao() {
 
         return new Negociacao(
