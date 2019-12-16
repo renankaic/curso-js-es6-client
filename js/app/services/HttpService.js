42bd9adb10d9160b6c4433d32118fe098c43a634
Renan Kaic Lopes <hae.renan.lopes@hyundai-brasil.com>
2019-12-16 09:40:08 -0300

    Utilizando a fetchAPI para as requisições XMLHttpRequest

--- js/app/services/HttpService.js
+++ js/app/services/HttpService.js
@@ -1,42 +1,17 @@
 class HttpService {
 
+    _handleErrors(response) {
+
+        if (!response.ok) throw new Error(response.statusText);
+        return response;
+
+    }
+
     get(url) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-
-            xhr.open("GET", url);
-
-            xhr.onreadystatechange = () => {
-
-                /*
-                    0: requisição ainda não iniciada
-                    1: conexão com o servidor estabelecida
-                    2: requisição recebida
-                    3: processando requisição
-                    4: requisição concluída e a resposta está pronta 
-                */
-
-                if (xhr.readyState === 4) {
-
-                    if (xhr.status === 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-
-                    } else {
-
-                        reject(xhr.responseText);
-
-                    }
-
-                }
-
-            };
-
-            xhr.send();
-
-        });
+        return fetch(url)
+            .then(response => this._handleErrors(response))
+            .then(response => response.json());
 
     }
 
